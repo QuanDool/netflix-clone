b2c16143261e8c1fd5874b3987a9db4a0ab3dcdb
Quan Pham <quandool0104@gmail.com>
2023-04-12 03:05:22 -0500

    Add React-Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NarBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
