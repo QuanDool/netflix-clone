25db31f000e07eb0769432ef1190d42cdb0ab825
Quan Pham <quandool0104@gmail.com>
2023-04-13 17:40:25 -0500

    Refactor Movie Component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
 import React, { useEffect } from "react";
 import { useState } from "react";
+import Movie from "./Movie";
 
 function Row({ title, fetchURL }) {
 	const [movies, setMovies] = useState([]);
@@ -16,25 +16,12 @@ function Row({ title, fetchURL }) {
 		<div>
 			<h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
 			<div className="relative flex items-center ">
-				<div id={"slider"}>
+				<div
+					id={"slider"}
+					className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+				>
 					{movies.map((movie, index) => {
-						return (
-							<div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-								<img
-									className="w-full h-auto block"
-									src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-									alt={movie?.title}
-								/>
-								<div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-									<p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-										{movie?.title}
-									</p>
-									<p className="absolute top-4 left-4 text-gray-300">
-										{false ? <FaHeart /> : <FaRegHeart />}
-									</p>
-								</div>
-							</div>
-						);
+						return <Movie movie={movie} key={index} />;
 					})}
 				</div>
 			</div>
